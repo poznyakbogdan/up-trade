78234c9bef600173f0b5d5bafeffa47b16eb7ff8
bogdan.poznyak <bogdan.poznyak@whalemap.io>
2023-05-03 20:06:56 +0300

    use retries

--- Implementations/BalanceService.cs
+++ Implementations/BalanceService.cs
@@ -1,37 +1,89 @@
 using Interfaces;
+using Microsoft.Extensions.Logging;
 using Nethereum.Hex.HexTypes;
 using Nethereum.JsonRpc.Client;
 using Nethereum.RPC.Eth;
 using Nethereum.RPC.Eth.DTOs;
 using Nethereum.Util;
 using Nethereum.Web3;
+using Polly;
 
 namespace Implementations;
 
 public class BalanceService : IBalanceService
 {
+    private class InnerRequestErrorException : Exception
+    {
+        
+    }
+    
     private readonly IClient _client;
     private readonly IWeb3 _web3;
-
-    public BalanceService(IWeb3 web3)
+    private readonly ILogger<BalanceService> _logger;
+    
+    private readonly AsyncPolicy _batchRequestRetryPolicy = Policy
+        .Handle<InnerRequestErrorException>()
+        .WaitAndRetryAsync(
+            5,
+            retryAttempt => TimeSpan.FromSeconds(1 * retryAttempt)
+        );
+    
+    public BalanceService(IWeb3 web3, ILogger<BalanceService> logger)
     {
         _web3 = web3;
         _client = _web3.Client;
+        _logger = logger;
     }
 
     public async Task<Dictionary<string, decimal>> GetBalances(IEnumerable<string> addresses)
     {
         var batches = CreateBatches(addresses.ToList());
         var result = new RpcRequestResponseBatch();
+        var withError = new List<string>();
+        
         foreach (var batch in batches)
         {
-            var batchResult = await _client.SendBatchRequestAsync(batch);
-            result.BatchItems.AddRange(batchResult.BatchItems);
-            await Task.Delay(TimeSpan.FromSeconds(3));
+            var (ok, error) = await SendRequest(batch);
+            result.BatchItems.AddRange(ok);
+            withError.AddRange(error.Select(x => (string)x.RpcRequestMessage.Id));
         }
+        
+        try
+        {
+            while (withError.Any())
+            {
+                _logger.LogInformation("Responses with error: {count}", withError.Count);
+                batches = CreateBatches(withError);
+                foreach (var batch in batches)
+                {
+                    await _batchRequestRetryPolicy.ExecuteAsync(async () =>
+                    {
+                        var (ok, errored) = await SendRequest(batch);
+                        withError = withError.Except(ok.Select(x => (string)x.RpcRequestMessage.Id)).ToList();
+                        result.BatchItems.AddRange(ok);
+                        if (!ok.Any()) throw new InnerRequestErrorException();
+                    });
+                } 
+            }        
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error during fetching balances");
+            throw;
+        }
+
         return BuildResult(result);
     }
-    
+
+    private async Task<(List<IRpcRequestResponseBatchItem>, List<IRpcRequestResponseBatchItem>)> SendRequest(RpcRequestResponseBatch batch) 
+    {
+        var batchResult = await _client.SendBatchRequestAsync(batch);
+        
+        return (
+            batchResult.BatchItems.Where(x => !x.HasError).ToList(),
+            batchResult.BatchItems.Where(x => x.HasError).ToList());
+    }
+
     private Dictionary<string, decimal> BuildResult(RpcRequestResponseBatch batch)
     {
         var res =  batch.BatchItems
