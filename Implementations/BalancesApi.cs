d7e82be55d596da2728f7e67e82fc19c1e7e9835
bogdan.poznyak <bogdan.poznyak@whalemap.io>
2023-05-03 19:07:23 +0300

    log response if error status code

--- Implementations/BalancesApi.cs
+++ Implementations/BalancesApi.cs
@@ -1,6 +1,7 @@
 using System.Net.Http.Json;
 using System.Text;
 using System.Text.Json;
+using Microsoft.Extensions.Logging;
 
 namespace Implementations;
 
@@ -8,17 +9,25 @@ public class BalancesApi
 {
     private const string BalancesUrl = "balances";
     private readonly HttpClient _httpClient;
+    private readonly ILogger<BalancesApi> _logger;
 
-    public BalancesApi(HttpClient httpClient)
+    public BalancesApi(HttpClient httpClient, ILogger<BalancesApi> logger)
     {
         _httpClient = httpClient;
+        _logger = logger;
     }
 
     public async Task<Dictionary<string, decimal>> GetBalances(IEnumerable<string> addresses)
     {
         var stringContent = JsonSerializer.Serialize(addresses.ToList());
         var response = await _httpClient.PostAsync(BalancesUrl, new StringContent(stringContent, Encoding.UTF8, "application/json"));
-        response.EnsureSuccessStatusCode();
+
+        if (!response.IsSuccessStatusCode)
+        {
+            _logger.LogWarning(await response.Content.ReadAsStringAsync());
+            return null;
+        }
+
         var responseContent = await response.Content.ReadAsStringAsync();
         var result = JsonSerializer.Deserialize<Dictionary<string, decimal>>(responseContent);
         return result;
